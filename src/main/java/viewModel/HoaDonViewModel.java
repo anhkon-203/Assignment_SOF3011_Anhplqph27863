944081216ec0d675e0cc4ccd0496276eed6cdc6f
Anhkon-203 <you@example.com>
2023-04-03 22:18:09 +0700

    test

---
+++ src/main/java/viewModel/HoaDonViewModel.java
@@ -0,0 +1,12 @@
+package viewModel;
+
+import java.math.BigDecimal;
+
+public class HoaDonViewModel {
+    private String tenSP;
+    private String srcImage;
+    private BigDecimal donGia;
+    private String soLuong;
+    private String tinhTrang;
+
+}
